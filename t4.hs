e3056c89e7b2b0b2548a50cc6ed7758f60adfa47
viniazzolin <viniazzolin@gmail.com>
2015-08-29 17:28:11 -0300

    Create t4.hs

---
+++ t4.hs
@@ -0,0 +1,37 @@
+-- Nome: Vinícius Stivanin Azzolin			ELC117 - Paradigmas de Programação
+-- Profª Andrea Schwertner Charão           Trabalho 04
+
+-- Exercício 01
+
+eleva2 :: [Int] -> [Int]
+eleva2 [] = []
+eleva2 (x:xs) = x^2 : eleva2(xs)
+
+-- Exercício 02
+
+contido :: Char -> [Char] -> Bool
+contido c [] = False
+contido c (x:xs) = if c == x then True else contido c xs
+
+-- Exercício 03
+
+semVogais :: [Char] -> [Char]
+semVogais [] = []
+semVogais (x:xs) = if (x=='a')||(x=='e')||(x=='i')||(x=='o')||(x=='u')||(x=='A')||(x=='E')||(x=='I')||(x=='O')||(x=='U') then semVogais xs else x:semVogais xs 
+
+-- Exercício 04
+
+--translate :: [(Int, Int)] -> [(Int, Int)]
+--translate [] = []
+--translate tup = take 1 tup + [(2,2)] ++ translate (drop 1 tup) 
+
+
+-- Exercício 05
+
+geraTabela' :: Int -> [(Int, Int)]
+geraTabela' n = aux 1 n
+
+aux :: Int -> Int -> [(Int, Int)]
+aux e n = if (e <= n)
+then (e, e^2) : aux (e+1) n
+else []
