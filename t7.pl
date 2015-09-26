63b93a3058eb31638d002391c3576d5041dac493
viniazzolin <viniazzolin@gmail.com>
2015-09-26 19:20:56 -0300

    t7.pl

--- t7.pl
+++ t7.pl
@@ -39,13 +39,17 @@ intercala(A,[L1|T1],[L1,A|T2]) :- intercala(A,T1,T2).
 % aux1(N, A,[H|T],[L,T1]) :- N mod 2 =:= 0, L is H, P is N+1, aux1(P,A,T,T1).
     
 % Exercício 8
-%comissao(NP,LP,C)
+comissao(0,_,[]).
+comissao(NP,LP,[C|T]) :- NP > 0, elem(C,LP,R), NP1 is NP-1, comissao(NP1,R,T).
+
+elem(X,[X|L],L).
+elem(X,[_|L],R) :- el(X,L,R).
 
 
 % Exercício 9
 azulejos(NA,NQ) :- aux2(0,NA,NQ).
 
 aux2(_,0,_). 
-aux2(N,NA,NQ):-  A is floor(sqrt(NA)), B is NA-(A*A), C is N+1, aux2(C,B,C).
+aux2(N,NA,NQ):-  A is floor(sqrt(NA)), B is NA-(A*A), aux2(N+1,B,N+1).
 
 
